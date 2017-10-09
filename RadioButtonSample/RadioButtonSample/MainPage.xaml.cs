ca4ffccc5915256bf65a9414b078eced56b98fb0
Jorge Ramirez <ramirez.jorgea@outlook.com>
2017-10-09 14:26:07 -0700

    - Initial check-in of my implementation with RadioButtons using DataTriggers and Data Binding with a single field in a ViewModel

---
+++ RadioButtonSample/RadioButtonSample/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace RadioButtonSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ RadioButtonSample/RadioButtonSample/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RadioButtonSample
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        int option;
+        public int Option
+        {
+            get { return option; }
+            set { option = value; OnPropertyChanged("Option"); }
+        }
+
+        public ICommand OptionCommand { protected set; get;  }
+
+        public MainViewModel()
+        {
+            Option = 1;
+
+            OptionCommand = new Command<string>((opt) => OptionCommandExecute(opt));
+        }
+
+        void OptionCommandExecute(string opt)
+        {
+            Option = int.Parse(opt);
+        }
+
+        #region INotifyPropertyChanges Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+            changed.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
