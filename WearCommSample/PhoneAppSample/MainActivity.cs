4f8f3963bd63b3a8ec83a5230dd6ea22c85a8d83
Jorge Ramirez <ramirez.jorgea@outlook.com>
2017-10-11 22:21:38 -0700

    Initial check-in:  - Added GooglePlayServices Wearable nuget packages on both projects  - Add Android Support libraries (including wearable) nuget packages  - Ensure both projects have the same Namespace  - Ensure both projects have the same package id in their manifest files  - Add meta-data android value for com.google.android.gms.version in the manifest for both projects  - Added layouts with a button to increase a TapCounter variable on both projects

---
+++ WearCommSample/PhoneAppSample/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace WearCommSample
+{
+    [Activity(Label = "PhoneAppSample", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        int TapCount;
+        Button btnTapCounter;
+        TextView lblTapCounter;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            btnTapCounter = FindViewById<Button>(Resource.Id.btnTapCounter);
+            lblTapCounter = FindViewById<TextView>(Resource.Id.lblTapCounter);
+
+            btnTapCounter.Click += BtnTapCounterClick;
+        }
+
+        void BtnTapCounterClick(object sender, System.EventArgs e)
+        {
+            TapCount++;
+            lblTapCounter.Text = TapCount.ToString();
+        }
+
+        protected override void OnDestroy()
+        {
+            btnTapCounter.Click -= BtnTapCounterClick;
+
+            base.OnDestroy();
+        }
+    }
+}
+
