c887204c9c0f6506f0bb48c7804f9c25f4d8333b
Jorge Ramirez <ramirez.jorgea@outlook.com>
2017-10-19 22:02:39 -0700

    - Updated project to latest Xamarin.Forms nuget package (version 2.4.0.282) - Added Xamarin Effect for iOS

---
+++ StatusBarEffectDemo/StatusBarEffectDemo/StatusBarEffectDemo.iOS/MyStatusBarEffect.iOS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Foundation;
+using StatusBarEffectDemo.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ResolutionGroupName("Xamarin")]
+[assembly: ExportEffect(typeof(MyStatusBarEffect), "StatusBarEffect")]
+namespace StatusBarEffectDemo.iOS
+{
+    public class MyStatusBarEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            UIView statusBar = UIApplication.SharedApplication.ValueForKey(new NSString("statusBar")) as UIView;
+            if (statusBar.RespondsToSelector(new ObjCRuntime.Selector("setBackgroundColor:")))
+            {
+                statusBar.BackgroundColor = StatusBarEffect.GetBackgroundColor().ToUIColor();
+            }
+        }
+
+        protected override void OnDetached()
+        {
+
+        }
+    }
+}
