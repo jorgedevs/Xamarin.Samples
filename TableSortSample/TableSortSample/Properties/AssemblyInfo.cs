f9e2fdcf6ca56529d5649b808aa0792178bc925f
Jorge Ramirez <ramirez.jorgea@outlook.com>
2018-03-30 22:51:21 -0700

    [TableSortSample] Project upgraded from PCL to .Net Standard

--- TableSortSample/TableSortSample/Properties/AssemblyInfo.cs
+++ TableSortSample/TableSortSample/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("TableSortSample")]
+//[assembly: AssemblyTitle("TableSortSample")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("(c) Jorge Ramirez")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
